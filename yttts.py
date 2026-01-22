e1f61741291d0f86c5aaafb52ebb0c6bc6e1b1b8
D0KT0R007 <kleinhannes2000@gmail.com>
2026-01-22 22:36:38 +0100

    Add missing arg and todos

--- yttts.py
+++ yttts.py
@@ -9,10 +9,17 @@ import sys
 
 
 if __name__ == "__main__":
+    
+    if len(sys.argv)<1:
+        sys.exit("missing link argument")
     link=sys.argv[1]
     id=re.search("(?<=watch\\?v=).*",link).group()
     print("Connecting to "+link+"with id: "+id)
+    
+    #todo add error handling maybe
     speak = Dispatch("SAPI.SpVoice").Speak
+    
+    #todo add error handling 
     chat = pytchat.create(video_id=id)
     while chat.is_alive():
         messageFeed=json.loads(chat.get().json())
