f9f5d8248f3c7c71af9ba06b66fb5732c0432a44
Hannes Klein <97946235+D0KT0R007@users.noreply.github.com>
2026-01-22 10:55:02 +0100

    Added Script

---
+++ yttts.py
@@ -0,0 +1,23 @@
+import pytchat
+import time
+import json
+import re
+from win32com.client import Dispatch
+import sys
+
+
+
+
+if __name__ == "__main__":
+    link=sys.argv[1]
+    id=re.search("(?<=watch\\?v=).*",link).group()
+    print("Connecting to "+link+"with id: "+id)
+    speak = Dispatch("SAPI.SpVoice").Speak
+    chat = pytchat.create(video_id=id)
+    while chat.is_alive():
+        messageFeed=json.loads(chat.get().json())
+        if len(messageFeed)>0:
+            authorName=re.search("(?<=@).*",(messageFeed[0]["author"]["name"])).group()
+            message=messageFeed[0]["message"]
+            print(authorName + " - " + message)
+            speak(authorName + " .-. " + message)
