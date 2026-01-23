0fbeb5042330714789fc75ea3a37ab827648240f
D0KT0R007 <kleinhannes2000@gmail.com>
2026-01-23 14:41:11 +0100

    added id-Error Message + Sleep-time

--- yttts.py
+++ yttts.py
@@ -15,16 +15,18 @@ if __name__ == "__main__":
     link=sys.argv[1]
     id=re.search("(?<=watch\\?v=).*",link).group()
     print("Connecting to "+link+"with id: "+id)
-    
-    #todo add error handling maybe
-    speak = Dispatch("SAPI.SpVoice").Speak
-    
-    #todo add error handling 
-    chat = pytchat.create(video_id=id)
+    voice = Dispatch("SAPI.SpVoice")
+    try:
+        chat = pytchat.create(video_id=id)
+    except:
+        sys.exit("Invalid youtube-link")
     while chat.is_alive():
         messageFeed=json.loads(chat.get().json())
         if len(messageFeed)>0:
             authorName=re.search("(?<=@).*",(messageFeed[0]["author"]["name"])).group()
             message=messageFeed[0]["message"]
             print(authorName + " - " + message)
-            speak(authorName + " .-. " + message)
+            voice.Speak(authorName + " .-. " + message)
+            voice.speakWaitUntilDone(-1)
+        else: time.sleep(5)
+
