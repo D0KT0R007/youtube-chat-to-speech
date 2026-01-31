d7f1ac253bda5957728e0c73d37c51ecc5d973b9
D0KT0R007 <kleinhannes2000@gmail.com>
2026-01-31 11:28:33 +0100

    Planned Functions

--- yttts.py
+++ yttts.py
@@ -6,12 +6,38 @@ from win32com.client import Dispatch
 import sys
 
 
+class Message ():
+    def __init__(self, authorName, text):
+        self.authorName=authorName
+        self.text=text
+
+
+
+def makeMessage (authorName, text):
+        
+    return Message(authorName,text)
+
+def checkMessage (Message):
+    #read Json
+
+    #check Names
+
+    if True: return
+    elif False: 
+        changedMessage=changeMessage(Message)
+        checkMessage(changedMessage)
+
+def changeMessage (Message, jsonObject):
+
+    return Message
+
+
 
 
 if __name__ == "__main__":
     
     if len(sys.argv)<1:
-        sys.exit("missing link argument")
+        sys.exit("Invalid Argument")
     link=sys.argv[1]
     id=re.search("(?<=watch\\?v=).*",link).group()
     print("Connecting to "+link+"with id: "+id)
